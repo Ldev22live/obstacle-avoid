405259ec5a569813bc310d1c72250ee6fa3d6bef
ldev22 <ldev22@live.com>
2026-04-29 18:20:15 +0200

    Fix

--- core/src/main/java/io/github/ldev22/assets/caselist/SnowflakeConnectionFactory.cs
+++ core/src/main/java/io/github/ldev22/assets/caselist/SnowflakeConnectionFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Ade.Club51.Case.List.Abstractions;
+using Microsoft.Extensions.Configuration;
 using Snowflake.Data.Client;
 using System;
 using System.Collections.Generic;
@@ -11,11 +12,18 @@ namespace Ade.Club51.Case.List.Helpers
 {
     public class SnowflakeConnectionFactory : ISnowflakeConnectionFactory
     {
+        private readonly IConfiguration _configuration;
+
+        public SnowflakeConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
         public IDbConnection CreateConnection()
         {
             IDbConnection conn = new SnowflakeDbConnection
             {
-                ConnectionString = Environment.GetEnvironmentVariable("SnowflakeConnectionString")
+                ConnectionString = _configuration["SnowflakeConnectionString"]
             };
             return conn;
         }
